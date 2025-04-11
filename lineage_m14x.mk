704f3b7d6e7cf78ddc4b20a62b2a1765480c69a2
devhunter1 <anubhabkundu2@gmail.com>
2025-04-11 13:03:07 +0530

    m14x: Update blobs from M146BXXS8CYB2

--- lineage_m14x.mk
+++ lineage_m14x.mk
@@ -36,3 +36,9 @@ TARGET_SCREEN_HEIGHT := 2408
 TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="m14xnsxx-user 14 UP1A.231005.007 M146BXXS8CYB2 release-keys" \
+    BuildFingerprint=samsung/m14xnsxx/essi:14/UP1A.231005.007/M146BXXS8CYB2:user/release-keys \
+    DeviceProduct=m14xnsxx \
+    SystemName=m14xnsxx
