d5746cfd43af5254ce01404ffb74e8bdf322aff0
devhunter1 <anubhabkundu2@gmail.com>
2025-06-17 20:41:24 +0530

    m14x: add dependency for hardware/samsung
    * needed for ril blobs

--- extract-files.py
+++ extract-files.py
@@ -21,6 +21,7 @@ from extract_utils.main import (
 namespace_imports = [
     'vendor/samsung/universal8535-common',
     'device/samsung/universal8535-common',
+    'hardware/samsung',
 ]
 
 def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
