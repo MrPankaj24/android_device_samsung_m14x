82d7d63e5de152ada7dde67d598f330d8f57d61e
devhunter1 <anubhabkundu2@gmail.com>
2025-08-12 20:26:47 +0530

    m14x: include mixer_path and mixer_gain on device tree

--- device.mk
+++ device.mk
@@ -23,6 +23,11 @@ $(call inherit-product, device/samsung/s5e8535-common/common.mk)
 
 # Inherit from the m14x vendor
 $(call inherit-product, vendor/samsung/m14x/m14x-vendor.mk)
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
+    $(DEVICE_PATH)/configs/audio/mixer_gains.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_gains.xml
     
 # Load kernel firmwares
 PRODUCT_COPY_FILES += \
